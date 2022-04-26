e2c40ec086dda464fb5836517696c62112aaeb0a
PROVILLE Maximilien <maximilien.proville77@gmail.com>
2022-04-26 13:10:15 +0200

    Done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer className="bg-gray-800  w-full top-0 z-10 mt-10">
+    <footer className="bg-gray-800  w-full z-10 mt-10 fixed bottom-0">
       <div className="flex flex-row p-5 justify-between sm:text-lg font-bold text-purple-300">
         <p className=" m-auto">iFollow : Cat Fact Test </p>
       </div>
